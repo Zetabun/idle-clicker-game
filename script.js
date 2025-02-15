bbb98a35a7962579a545ad9cf5800b9a7ae55105
Zetabun <adambullas@me.com>
2025-02-15 04:30:35 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -616,6 +616,14 @@
     }
 
     updateWeather(deltaTime);
+	
+	function formatNumber(num) {
+  if (num < 1000) return num.toFixed(0);
+  let exponent = Math.floor(Math.log10(num));
+  let mantissa = num / Math.pow(10, exponent);
+  return mantissa.toFixed(2) + "e" + exponent;
+}
+
 
     // Auto-clickers
     autoTickProgress += deltaTime;
