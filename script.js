c207d29daec6d98bbf77c4e1449d36bb91c8fbb6
Zetabun <adambullas@me.com>
2025-02-16 02:02:21 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1115,7 +1115,7 @@ function applyCarOfflineProgress(offlineSeconds) {
         game.car.techTokens += tokensGained;
         game.car.tokenProgress -= tokensGained * game.car.tokenThreshold;
     }
-}  // ✅ Correctly closing the function here
+
 
 
 // Helper function to force a full reload (cache-busting)
